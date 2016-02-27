d968bcfd42463bcbc4cddf891ae76df85c45d6f2
Alexander Hoereth <a.hoereth@gmail.com>
2016-02-27 19:26:35 +0100

    Connect router history to redux.

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,11 +1,16 @@
 import { createStore as _createStore, applyMiddleware } from 'redux';
+import { browserHistory } from 'react-router';
+import { routerMiddleware, } from 'react-router-redux';
 
 import fetchMiddleware from './middleware/fetchMiddleware';
 import reducer from './modules/reducer';
 
 
 export default function createStore(client, data) {
-  const middlewares = [fetchMiddleware(client)];
+  const middlewares = [
+    fetchMiddleware(client),
+    routerMiddleware(browserHistory),
+  ];
   const finalCreateStore = applyMiddleware(...middlewares)(_createStore);
   const store = finalCreateStore(reducer, data);
 
