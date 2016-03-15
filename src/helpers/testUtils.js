04b91a05b21d483ecc28a52cea46281cc91e312b
Alexander Hoereth <a.hoereth@gmail.com>
2016-03-15 17:21:57 +0100

    Add more and document utility functions.

--- src/helpers/testUtils.js
+++ src/helpers/testUtils.js
@@ -1,5 +1,12 @@
 import { createRenderer } from 'react-addons-test-utils';
 
+
+/**
+ * Shallowly renders a component for testing.
+ * 
+ * @param  {ReactElement} componentInstance
+ * @return {ReactElement}
+ */
 export function shallowRender(componentInstance) {
   let renderer = createRenderer();
   renderer.render(componentInstance);
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,12 +1,3 @@
-export function startsWith(haystack, needle) {
-  return (haystack.indexOf(needle) === 0);
-}
-
-export function endsWith(haystack, needle) {
-  return (haystack.indexOf(needle) === (haystack.length - needle.length));
-}
-
-
 /**
  * Check if given value is a string.
  *
@@ -15,6 +6,22 @@ export function endsWith(haystack, needle) {
  */
 export function isString(val) {
   return toString.call(val) == '[object String]';
+}
+
+
+/**
+ * Check if given value is a javascript object.
+ *
+ * @see http://stackoverflow.com/a/22482737
+ * @param  {any}  val
+ * @return {Boolean}
+ */
+export function isObject(val) {
+  if (val === null) {
+    return false;
+  }
+
+  return ( (typeof val === 'function') || (typeof val === 'object') );
 }
 
 
@@ -28,6 +35,38 @@ export function isUndefined(x) {
   return (typeof x === 'undefined');
 }
 
+
+/**
+ * Check if a string starts with a specific substring.
+ *
+ * @param  {string} haystack
+ * @param  {string} needle
+ * @return {boolean}
+ */
+export function startsWith(haystack, needle) {
+  return (haystack.indexOf(needle) === 0);
+}
+
+
+/**
+ * Checks if a string ends with a specific substring.
+ *
+ * @param  {string} haystack
+ * @param  {string/array} needle
+ * @return {boolean}
+ */
+export function endsWith(haystack, needle) {
+  return (haystack.indexOf(needle) === (haystack.length - needle.length));
+}
+
+
+/**
+ * Slugifies a string, converting it to lower case and replacing all special
+ * characters (besides umlauts!) and spaces with dashes.
+ *
+ * @param  {string} str
+ * @return {string}
+ */
 export function slugify(str) {
   return str.toLowerCase().replace(/[^\wüöä]+/ig, '-');
 }
