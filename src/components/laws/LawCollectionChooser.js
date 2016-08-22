923e723b701bb382c87ff811a2a4b861e4e6ec36
Alexander Höreth <a.hoereth@gmail.com>
2016-08-22 14:18:57 +0200

    Implement shells for law index collection chooser.

--- src/components/laws/LawCollectionChooser.js
+++ src/components/laws/LawCollectionChooser.js
@@ -1,16 +1,28 @@
 import React, { PropTypes } from 'react';
+import { List } from 'immutable';
+import { range } from 'lodash';
 import ImmutableTypes from 'react-immutable-proptypes';
 import { Grid, Cell, Button } from 'react-mdl';
 
 
+const shell = List(range(4).map(() => ''));
+
+
 const LawCollectionChooser = ({ collections, selected, onSelect }) => (
-  <Grid noSpacing>
-    {collections.map(title => (
-      <Cell key={title} col={3} tablet={4} phone={1}>
+  <Grid>
+    {(collections.size ? collections : shell).map((title, idx) => (
+      <Cell
+        key={title || `shell-${idx}`}
+        col={3}
+        tablet={4}
+        phone={4}
+      >
         <Button
           ripple raised
+          disabled={title === ''}
           accent={selected === title}
           onClick={() => onSelect(selected !== title ? title : '')}
+          style={{ width: '100%' }}
         >
           {title}
         </Button>
