231ff9ff782624e1e6ac976fc3ccd6e0884e04b5
Alexander Höreth <a.hoereth@gmail.com>
2016-08-22 16:24:38 +0200

    Implement shell server.

---
+++ scripts/shellserver.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-commonjs */
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const PATH = path.resolve(__dirname, '..', 'dist');
+
+
+http.createServer((req, res) => {
+  const { url } = req;
+
+  let file = 'index.html';
+
+  if (url.indexOf('/static') === 0) {
+    file = url.slice(1);
+  } else if (url.indexOf('/gesetze') === 0) {
+    file = 'gesetze/index.html';
+  } else if (url.indexOf('/gesetz') === 0) {
+    file = 'gesetz/index.html';
+  }
+
+  fs.readFile(path.resolve(PATH, file), (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+}).listen(8080, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  // eslint-disable-next-line no-console
+  console.log('Listening on 8080...');
+});
