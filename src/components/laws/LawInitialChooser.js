a38df81108359a96fb9fa0b5b5e16e42a3424177
Alexander Höreth <a.hoereth@gmail.com>
2016-08-22 14:03:42 +0200

    Implement shells for law index initial chooser.

--- src/components/laws/LawInitialChooser.js
+++ src/components/laws/LawInitialChooser.js
@@ -1,18 +1,29 @@
 import React, { PropTypes } from 'react';
+import { List } from 'immutable';
+import { range } from 'lodash';
 import ImmutableTypes from 'react-immutable-proptypes';
 import { Grid, Cell, Button } from 'react-mdl';
 
 import styles from './lawInitialChooser.sss';
 
 
+const shell = List(range(20).map(() => ''));
+
+
 const LawInitialChooser = ({ initials, selected, onSelect }) => (
-  <Grid noSpacing>
-    {initials.map(char => (
-      <Cell key={char} col={12} tablet={8} phone={1} className={styles.cell}>
+  <Grid>
+    {(initials.size ? initials : shell).map((char, idx) => (
+      <Cell
+        key={char || `shell-${idx}`}
+        col={12}
+        tablet={8}
+        phone={1}
+        className={styles.cell}
+      >
         <Button
           ripple raised
+          disabled={char === ''}
           accent={selected === char.toLowerCase()}
-          className={styles.button}
           onClick={() => onSelect(selected !== char.toLowerCase() ? char : '')}
         >
           {char}
