bc742008156512c71bc1901ab997e00c44682f96
Alexander Höreth <ahoereth@gmail.com>
2016-09-14 15:34:11 +0200

    Allow search submission by pressing enter.

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -2,10 +2,17 @@ import React, { PropTypes } from 'react';
 import { Textfield } from 'react-mdl';
 
 
+function submit({ nativeEvent, currentTarget }, handler) {
+  if (nativeEvent.charCode === 13) {
+    handler(currentTarget.value);
+  }
+}
+
 const SearchInput = ({ expandable, query, search, ...otherProps }) => (
   <Textfield
     value={query}
-    onChange={(e) => search(e.currentTarget.value)}
+    onChange={e => search(e.currentTarget.value)}
+    onKeyPress={e => submit(e, search)}
     label='Suchanfrage'
     expandable={expandable}
     expandableIcon='search'
